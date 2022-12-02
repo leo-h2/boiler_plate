836ba56afbaca499a9208898990737aa776b35fd
조범래 <leo.cho082@jobeomlaeui-MacBookPro.local>
2022-12-03 00:09:24 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const express = require('express') // 모듈을 갖고 온다는 뜻
 const app = express()
 const port = 3000
+
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const {User} = require('./models/User');
+
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb+srv://leo-cho:1001@cluster0.ddlcz.mongodb.net/?retryWrites=true&w=majority', {
-//     useNewUrlParser :true, useUnifiedTopology:true , useCreateIndex :true, useFindAndModify:false
-// }).then(()=> console.log('MongoDB Connected'))
-//   .catch(err =>console.log(err))
+// application x-wwww-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// application/json
+app.use(bodyParser.json());
 
-
-  mongoose.connect('mongodb+srv://leo-cho:1001@cluster0.ddlcz.mongodb.net/?retryWrites=true&w=majority', {
+  mongoose.connect(config.mongoURI, {
     useNewUrlParser :true, useUnifiedTopology:true
   }).then(()=> console.log('MongoDB Connected'))
   .catch(err =>console.log(err))
 
 
+app.get('/', (req,res) => res.send('hellow world 안녕하세요 새해복 많이 받으세요') )
 
-app.get('/', (req,res) => res.send('hellow world 안녕하세요') )
+app.post('/register', (req, res)=>{
+// 회원 가입할 때 필요한 정보들을 client 에서 가져오면 그것들을 데이터 베이스에 넣어 준다.
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+      sucess:true
+    })
+  })
+})
+
 app.listen(port, () => console.log('example app listening on port ${port}!'))
 
 
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        maxlength:50
+    },
+    email:{
+        type:String,
+        trim:true, // 빈칸이 있는 경우 없애 주는 역할을 함
+        unique:1 // 
+    },
+    password:{
+        type:String,
+        minlength:5
+    },
+    lastname:{
+        type:String,
+        maxlength:50
+    },
+    role:{
+        type:Number, // 권한 관리용
+        default:0
+    },
+    image:String,
+    token:{
+        type:String
+    },
+    tokenExp:{
+        type:Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = {User}
